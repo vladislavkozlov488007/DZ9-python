efea61d727014f34815dff664848c083f8321dc2
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2023-06-02 16:09:20 +0300

    Add files via upload

---
+++ DZ9.py
@@ -0,0 +1,16 @@
+Задание
+Задача 40: Работать с файлом california_housing_train.csv, который находится в папке
+sample_data. Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population)
+Решение:
+​
+df[df['population']<501]['median_house_value'].agg(['mean'])
+# mean    206799.951402
+# Name: median_house_value, dtype: float64
+
+
+Задача 42: Узнать какая максимальная households в зоне минимального значения population
+
+Решение:
+​
+df[df['population']==df['population'].min()]['households'].max()
+# 4.0
